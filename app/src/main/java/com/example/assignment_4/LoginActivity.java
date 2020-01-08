df0574748bd78118e283d09b8cde9e4addb30aff
Humendra Pun <xantoshpun@gmail.com>
2020-01-09 00:23:36 +0545

    done

--- app/src/main/java/com/example/assignment_4/LoginActivity.java
+++ app/src/main/java/com/example/assignment_4/LoginActivity.java
@@ -46,7 +46,8 @@ public class LoginActivity extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
             }
         });
     }
