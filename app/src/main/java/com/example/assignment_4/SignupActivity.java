84efee7a7623471b5ac2a28ce90554224b340838
Humendra Pun <xantoshpun@gmail.com>
2020-01-09 00:20:00 +0545

    done

--- app/src/main/java/com/example/assignment_4/SignupActivity.java
+++ app/src/main/java/com/example/assignment_4/SignupActivity.java
@@ -1,14 +1,160 @@
 package com.example.assignment_4;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class SignupActivity extends AppCompatActivity {
+
+    RelativeLayout create, name, birthday, gender, mobile, pass, em, terms;
+    Button next1,next2,next3,next4,next5,next6,next7,next8;
+    TextView menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        create = findViewById(R.id.create);
+        name = findViewById(R.id.name);
+        birthday = findViewById(R.id.birthday);
+        gender = findViewById(R.id.gender);
+        mobile = findViewById(R.id.mobile);
+        pass = findViewById(R.id.pass);
+        em = findViewById(R.id.em);
+        terms = findViewById(R.id.terms);
+
+        menu = findViewById(R.id.menu);
+
+        next1 = findViewById(R.id.next1);
+        next2 = findViewById(R.id.next2);
+        next3 = findViewById(R.id.next3);
+        next4 = findViewById(R.id.next4);
+        next5 = findViewById(R.id.next5);
+        next6 = findViewById(R.id.next6);
+        next7 = findViewById(R.id.next7);
+        next8 = findViewById(R.id.next8);
+
+        next1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menu.setText("Name");
+                create.setVisibility(View.GONE);
+                name.setVisibility(View.VISIBLE);
+                birthday.setVisibility(View.GONE);
+                gender.setVisibility(View.GONE);
+                mobile.setVisibility(View.GONE);
+                pass.setVisibility(View.GONE);
+                em.setVisibility(View.GONE);
+                terms.setVisibility(View.GONE);
+
+            }
+        });
+
+        next2.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menu.setText("Birthday");
+                create.setVisibility(View.GONE);
+                name.setVisibility(View.GONE);
+                birthday.setVisibility(View.VISIBLE);
+                gender.setVisibility(View.GONE);
+                mobile.setVisibility(View.GONE);
+                pass.setVisibility(View.GONE);
+                em.setVisibility(View.GONE);
+                terms.setVisibility(View.GONE);
+            }
+        }));
+
+        next3.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menu.setText("Gender");
+                create.setVisibility(View.GONE);
+                name.setVisibility(View.GONE);
+                birthday.setVisibility(View.GONE);
+                gender.setVisibility(View.VISIBLE);
+                mobile.setVisibility(View.GONE);
+                pass.setVisibility(View.GONE);
+                em.setVisibility(View.GONE);
+                terms.setVisibility(View.GONE);
+            }
+        }));
+
+        next4.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menu.setText("Mobile Number");
+                create.setVisibility(View.GONE);
+                name.setVisibility(View.GONE);
+                birthday.setVisibility(View.GONE);
+                gender.setVisibility(View.GONE);
+                mobile.setVisibility(View.VISIBLE);
+                pass.setVisibility(View.GONE);
+                em.setVisibility(View.GONE);
+                terms.setVisibility(View.GONE);
+            }
+        }));
+
+        next5.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menu.setText("Password");
+                create.setVisibility(View.GONE);
+                name.setVisibility(View.GONE);
+                birthday.setVisibility(View.GONE);
+                gender.setVisibility(View.GONE);
+                mobile.setVisibility(View.GONE);
+                pass.setVisibility(View.VISIBLE);
+                em.setVisibility(View.GONE);
+                terms.setVisibility(View.GONE);
+            }
+        }));
+
+        next6.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menu.setText("Email");
+                create.setVisibility(View.GONE);
+                name.setVisibility(View.GONE);
+                birthday.setVisibility(View.GONE);
+                gender.setVisibility(View.GONE);
+                mobile.setVisibility(View.GONE);
+                pass.setVisibility(View.GONE);
+                em.setVisibility(View.VISIBLE);
+                terms.setVisibility(View.GONE);
+            }
+        }));
+
+        next7.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menu.setText("Terms & Condition");
+                create.setVisibility(View.GONE);
+                name.setVisibility(View.GONE);
+                birthday.setVisibility(View.GONE);
+                gender.setVisibility(View.GONE);
+                mobile.setVisibility(View.GONE);
+                pass.setVisibility(View.GONE);
+                em.setVisibility(View.GONE);
+                terms.setVisibility(View.VISIBLE);
+            }
+        }));
+
+        next8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignupActivity.this, "New Account created Successfully...!!!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignupActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
